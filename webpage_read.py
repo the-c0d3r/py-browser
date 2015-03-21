dba92bad7da2906419cdbc2d74934ed07ea2db89
Hadesy2k <Hadesy2k@users.noreply.github.com>
2015-03-21 23:53:35 +0630

    Update webpage_read.py

--- webpage_read.py
+++ webpage_read.py
@@ -2,9 +2,9 @@
 
 import urllib2, random, anonymize
 
-def readpg():
+def readpg(theurl):
     pgreader = urllib2.build_opener()
-    response = pgreader.open(usrurl)
+    response = pgreader.open(theurl)
     
 usrurl = raw_input("Enter a url:\n")
 readpg(usrurl)
