32fc59b8add86ddf41e209c8c2b30886131e4e93
the-c0d3r <z3r0.mhu@gmail.com>
2015-03-22 10:59:53 +0800

    Converted tabs to spaces.

--- pybrowser.py
+++ pybrowser.py
@@ -4,24 +4,24 @@ from PyQt4 import QtGui
 
 class Browser(QtGui.QMainWindow):
 
-	def __init__(self):
-		super(Browser,self).__init__()
+    def __init__(self):
+        super(Browser,self).__init__()
 
-		self.initUI()
+        self.initUI()
 
-	def initUI(self):
-		# Setting windows title
-		self.setWindowTitle("Untitled - PyBrowser")
-		# Moving windows to desktop center when opened by default
-		self.move(QtGui.QApplication.desktop().screen().rect().center()-self.rect().center())
-		self.statusBar()
-		self.resize(500,500)
-		self.show()
+    def initUI(self):
+        # Setting windows title
+        self.setWindowTitle("Untitled - PyBrowser")
+        # Moving windows to desktop center when opened by default
+        self.move(QtGui.QApplication.desktop().screen().rect().center()-self.rect().center())
+        self.statusBar()
+        self.resize(500,500)
+        self.show()
 
 def main():
-	app = QtGui.QApplication(sys.argv)
-	ex = Browser()
-	sys.exit(app.exec_())
+    app = QtGui.QApplication(sys.argv)
+    ex = Browser()
+    sys.exit(app.exec_())
 
 if __name__ == '__main__':
-	main()
+    main()
